79b17fbd40f5a7199450fb1796a50fce1ccb7689
NORTH KOREA <139649346+CarlosDan1ell@users.noreply.github.com>
2025-08-27 14:47:57 -0400

    Blynk lendo o nível do GPIO
    teve uma hora que nao tava funcionando, mas foi pq eu esqueci de verificar as conexões kkkkk

--- main.c
+++ main.c
@@ -2,24 +2,30 @@
 #include <driver/gpio.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#define LED GPIO_NUM_25
+#define BUTTON GPIO_NUM_16
 
-void blink_task(void *pvParameter){
-    
-    gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
-    printf("Blinking LED on GPIO 2\n");
+
+void Task_LED(void *pvParameter){
     int cnt = 0;
-    while (1)
-    {
-        gpio_set_level(GPIO_NUM_2,cnt%2);
-        cnt++;
-        vTaskDelay(100/portTICK_PERIOD_MS);
+    gpio_set_direction(BUTTON,GPIO_MODE_INPUT);
+    gpio_set_pull_mode(BUTTON,GPIO_PULLUP_ONLY);
+    gpio_set_direction(LED,GPIO_MODE_OUTPUT);
+    printf("Piscando o led com o pino 25\n");
+
+    while(1){
+        if (gpio_get_level(BUTTON)==0)
+        {
+            printf("%d\n", cnt%2);
+            gpio_set_level(LED,cnt%2);
+            cnt++;
+        }
+        
+        vTaskDelay(300/portTICK_PERIOD_MS);
+
     }
-    
-
 }
-void app_main(void)
-{
-    xTaskCreate(blink_task,"blink_task",1024,NULL,5,NULL);
-    printf("Blynk task started\n");
 
+void app_main(void){
+    xTaskCreate(Task_LED,"Task_LED",2048,NULL,1,NULL);
 }
