285b76e7e4669f697578d0591d3953051833331b
NORTH KOREA <139649346+CarlosDan1ell@users.noreply.github.com>
2025-08-30 14:17:04 -0400

    Farol com Interrupcao2
    Esse codigo eh mais atualizado, antes tinha um bug que se o led amarelo ficasse aceso, o vermelho nao ligava. Resolvi colocando um cnt pra cada e dentro de seus ifs e tambem utilizei struct para configurar o descritor dos pinos

---
+++ GPIOS/main.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <driver/gpio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#define LED_1 GPIO_NUM_25 //vermelho
+#define LED_2 GPIO_NUM_26 //amarelo
+#define LED_3 GPIO_NUM_27 //verde
+#define BUTTON_1 GPIO_NUM_16
+#define BUTTON_2 GPIO_NUM_22
+#define GPIO_OUTPUT_PIN_SEL ((1ULL<<LED_1)|(1ULL<<LED_2)|(1ULL<<LED_3))
+#define GPIO_INPUT_PIN_SEL ((1ULL<<BUTTON_1)|(1ULL<<BUTTON_2))
+#define ESP_INTR_FLAG_DEFAULT 0
+
+volatile  int cnt_1 = 0;
+volatile  int cnt_2 = 0;
+
+
+static void IRAM_ATTR gpio_isr_handler(void* arg){
+    //identifica qual botão foi presssionado
+    if(BUTTON_1 == (uint32_t) arg){
+        if(gpio_get_level((uint32_t)arg) ==0){ //primeiro botao acende o vermelho
+            gpio_set_level(LED_1,cnt_1%2);
+            cnt_1++;
+            
+
+        }
+    }
+    else {
+        if(BUTTON_2 == (uint32_t) arg){
+            if(gpio_get_level((uint32_t)arg) ==0){  //segundo botao acende o amarelo
+                gpio_set_level(LED_2,cnt_2%2);
+                cnt_2++;
+
+            }
+        }
+        
+
+    }
+}
+
+
+
+
+
+
+void Task_LED(void *pvParameter){
+    
+    //Configura o descritor de Outputs(LED)
+    gpio_config_t io_conf = {
+        .intr_type =  GPIO_INTR_DISABLE, 
+        .mode = GPIO_MODE_OUTPUT,
+        .pin_bit_mask = GPIO_OUTPUT_PIN_SEL,
+        //.pull_down_en = GPIO_PULLDOWN_DISABLE,
+        //.pull_up_en = GPIO_PULLUP_DISABLE,
+
+    };
+    
+    gpio_config(&io_conf); // configura as GPIOs conforme a primeira lista "io_conf"
+
+    //Configura o descritor de Inputs(botões)
+    gpio_config_t io_conf2 = {
+    .intr_type =  GPIO_INTR_NEGEDGE, //Habilita interrupção na borda de descida 
+    .mode = GPIO_MODE_INPUT,
+    .pin_bit_mask = GPIO_INPUT_PIN_SEL,
+    .pull_down_en = GPIO_PULLDOWN_DISABLE,
+    .pull_up_en = GPIO_PULLUP_ENABLE,
+    };
+
+    gpio_config(&io_conf2); //configura as GPIOs conforme a primeira lista "io_conf"
+
+    //Ao utilizar gpio_install_isr_service(); todas as interrupções de GPIO de descritor
+    //vão chamar a mesma interrupção. A função de callback que será chamada para cada interrupção
+    //é definida em gpio_isr_handler_add();
+    //o flag ESP_INTR_FLAG_DEFAULT tem a ver com a alocação do vetor de interrupção, que neste caso
+    //o valor zero informa para alocar no setor de interrupção não compartilhado de nível 1, 2 ou 3
+
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    gpio_isr_handler_add(BUTTON_2, gpio_isr_handler, (void*) BUTTON_2); //Registra a interrupção externa do BOTTON_1
+    gpio_isr_handler_add(BUTTON_1, gpio_isr_handler, (void*) BUTTON_1); //Registra a interrupção externa do BOTTON_2
+    
+    //função de interrupção 
+    
+    while(1){
+    //Aguardando a interrupção das GPIOs serem geradas...
+    vTaskDelay(300/portTICK_PERIOD_MS);   
+         
+
+    }
+}
+
+void app_main(void){
+    xTaskCreate(Task_LED,"Task_LED",2048,NULL,1,NULL);
+}
